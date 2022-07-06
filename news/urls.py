f246afbcfb1d524aa00e28b77d7eff52b7b7151b
kirill <operax0@yandex.ru>
2022-07-06 20:14:37 +0500

    transform

--- news/urls.py
+++ news/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import url, include
 #    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 
 urlpatterns = [
-    path('', views.ArticlesListView.as_view(), name='articles_list'),
+    path('', views.new, name='articles_list'),
     path('<slug:slug>', views.ArticlesDetailView.as_view(), name='articles_detail'),
     ]
+#views.ArticlesListView.as_view()
--- news/views.py
+++ news/views.py
@@ -3,8 +3,8 @@ from .models import Articles
 from django.views.generic import ListView, DetailView
 # Create your views here.
 def new(request):
-    news = Articles.objects.all()
-    return render(request, 'news/new.html', {'news': news})
+    news = Articles.objects.order_by("-date")
+    return render(request, 'news/articles_list.html', {'news': news})
 class ArticlesListView(ListView):
     model = Articles
     template_name = 'news/articles_list.html'
