69d1d32d5be53ab6029b03bf1c030bcf86785e01
kirill <operax0@yandex.ru>
2022-07-28 23:10:55 +0500

    aaaa

--- news/views.py
+++ news/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Articles
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 # Create your views here.
 def new(request):
-    news = Articles.objects.order_by("-date")
-    return render(request, 'news/articles_list.html', {'news': news})
+    new = Articles.objects.order_by("-date")
+    paginator = Paginator(new, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.get_page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'news/articles_list.html', {'posts': posts, 'page': page})
 class ArticlesListView(ListView):
     model = Articles
     template_name = 'news/articles_list.html'
